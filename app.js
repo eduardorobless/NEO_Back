327b8829924a15cbb42e73c3379e2b8ec3b726b3
Eduardo Robles <eduardo_robles14@hotmail.com>
2022-06-23 06:13:03 -0500

    response only needed

---
+++ app.js
@@ -0,0 +1,39 @@
+const axios = require('axios')
+const express = require('express')
+const cors = require('cors')
+const app = express()
+app.use(cors())
+const port = 3001
+
+require('dotenv').config()
+
+
+
+app.get('/neo', (req, res) => {
+    const start_date = req.query.start_date
+    const end_date = req.query.end_date
+    console.log(start_date)
+    console.log(end_date)
+    console.log(process.env.REACT_APP_NASA_API_KEY)
+
+    const options = {
+        method: 'GET', 
+        url: `https://api.nasa.gov/neo/rest/v1/feed?start_date=${start_date}&end_date=${end_date}&api_key=${process.env.REACT_APP_NASA_API_KEY}`
+    }
+
+
+    axios.request(options)
+    .then((response) => {
+        res.json(response.data.near_earth_objects)
+    })
+    .catch((error) => {
+        console.log('error')
+        console.log(error)
+    })
+
+})
+
+
+app.listen(port, () => {
+    console.log(`server listening on port ${port}`)
+})
