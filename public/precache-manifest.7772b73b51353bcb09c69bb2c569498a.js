e4599ba0945ab1857de244625576b84a40f24185
Eduardo Robles <eduardo_robles14@hotmail.com>
2022-06-25 04:19:58 -0500

    adding static react site

---
+++ public/precache-manifest.7772b73b51353bcb09c69bb2c569498a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7f804b0f3f6bf168e092",
+    "url": "/static/css/main.d1d06912.chunk.css"
+  },
+  {
+    "revision": "7f804b0f3f6bf168e092",
+    "url": "/static/js/main.49ca01d3.chunk.js"
+  },
+  {
+    "revision": "a1c14705520b8d6fba18",
+    "url": "/static/js/runtime~main.38a2c3be.js"
+  },
+  {
+    "revision": "af1cd4cf706a851fd73b",
+    "url": "/static/js/2.a74dd11c.chunk.js"
+  },
+  {
+    "revision": "b2871250ed127b512992",
+    "url": "/static/js/3.6f35523b.chunk.js"
+  },
+  {
+    "revision": "031f8c2d1dda642572fa79a08f7d1425",
+    "url": "/index.html"
+  }
+];
