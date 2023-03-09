40170be178db5d01773336223002f89d45e20343
Eduardo Robles <eduardo_robles14@hotmail.com>
2023-03-09 13:55:40 -0600

    main js was edited

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express')
 const cors = require('cors')
 const app = express()
 const path = require('path')
+const e = require('express')
 app.use(cors())
 app.use(express.static(path.join(__dirname + "/public")))
 const port = 5000
@@ -14,6 +15,14 @@ require('dotenv').config()
 app.get('/neo', (req, res) => {
     const start_date = req.query.start_date
     const end_date = req.query.end_date
+
+
+
+    console.log('start_date: ' + start_date ); 
+    console.log('end_date ' + end_date); 
+
+    console.log(process.env.REACT_APP_NASA_API_KEY); 
+
     const options = {
         method: 'GET', 
         url: `https://api.nasa.gov/neo/rest/v1/feed?start_date=${start_date}&end_date=${end_date}&api_key=${process.env.REACT_APP_NASA_API_KEY}`
@@ -23,10 +32,21 @@ app.get('/neo', (req, res) => {
     if (start_date === end_date) {
         axios.request(options)
         .then((response) => {
-            response = response.data.near_earth_objects[start_date].map(({ links, ...object }) => object);
+            if(response.data.near_earth_objects[
+                start_date
+            ] != undefined) {
+                response = response.data.near_earth_objects[start_date].map(({ links, ...object }) => object);
+            } else {
+                response = {}
+                response.error = 'No results found'; 
+            }
+
+
+
             res.json(response)
         })
         .catch((error) => {
+            console.log(error)
            res.status(400).json({"error": error})
         })
     }
